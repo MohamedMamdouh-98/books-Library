6520d01d3d07e23ffeb368d3781f301187235805
MohamedMamdouh-98 <miido96@gmail.com>
2022-09-13 14:47:26 +0200

    first commit

---
+++ src/components/Book/BookContainer.js
@@ -0,0 +1,28 @@
+import React, { Fragment, useEffect } from "react";
+import BookInfo from "./BookInfo";
+import BooksList from "./BooksList";
+import { useDispatch } from "react-redux";
+import { getBooks } from "../../store/bookSlice";
+import "./book.css";
+
+const PostContainer = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
+  return (
+    <Fragment>
+      <hr className="my-5" />
+      <div className="row">
+        <div className="col">
+          <BooksList />
+        </div>
+        <div className="col side-line">
+          <BookInfo />
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default PostContainer;
---
+++ src/components/Container.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Container = ({ children }) => {
+  return <div className='container py-5'>{children}</div>;
+};
+
+export default Container;

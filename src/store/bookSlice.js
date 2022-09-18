7ccd2f9224f0b3908ec684610c0cfae99d6ba3f1
MohamedMamdouh-98 <miido96@gmail.com>
2022-09-18 19:01:18 +0200

    loading from pending in extra reducers

--- src/store/bookSlice.js
+++ src/store/bookSlice.js
@@ -1,22 +1,38 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 //call Api
-const getBooks = createAsyncThunk("book/getBooks", async (_, thunkAPI) => {
-  try {
-    const res = await fetch("http://localhost:3005/books");
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.log(error);
+export const getBooks = createAsyncThunk(
+  "book/getBooks",
+  async (data, thunkAPI) => {
+    try {
+      const res = await fetch("http://localhost:3005/books");
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   }
-});
+);
 
 //create reducer
 const bookSlice = createSlice({
   name: "book",
-  initialState: { books: null },
-  reducers: {},
+  initialState: { books: [], isLoading: false },
+  extraReducers: {
+    [getBooks.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+
+    [getBooks.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.books = action.payload
+    },
+
+    [getBooks.rejected]: (state, action) => {
+      state.isLoading = false;
+      console.log(action);
+    },
+  },
 });
 
-export const {} = bookSlice.actions;
 export default bookSlice.reducer;

8b08b05823887b0c053b6eac39a1751a2f4f85c1
MohamedMamdouh-98 <miido96@gmail.com>
2022-09-19 12:07:53 +0200

    [handle]action error

--- src/store/bookSlice.js
+++ src/store/bookSlice.js
@@ -4,12 +4,13 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 export const getBooks = createAsyncThunk(
   "book/getBooks",
   async (data, thunkAPI) => {
+    const { rejectWethValue } = thunkAPI;
     try {
       const res = await fetch("http://localhost:3005/books");
       const data = await res.json();
       return data;
     } catch (error) {
-      console.log(error);
+      return rejectWethValue(error.message);
     }
   }
 );
@@ -17,19 +18,22 @@ export const getBooks = createAsyncThunk(
 //create reducer
 const bookSlice = createSlice({
   name: "book",
-  initialState: { books: [], isLoading: false },
+  initialState: { books: [], isLoading: false, error: null },
   extraReducers: {
     [getBooks.pending]: (state, action) => {
       state.isLoading = true;
+      state.error = null
     },
 
     [getBooks.fulfilled]: (state, action) => {
       state.isLoading = false;
-      state.books = action.payload
+      state.books = action.payload;
+      state.error = null
     },
 
     [getBooks.rejected]: (state, action) => {
       state.isLoading = false;
+      state.error = action.payload;
       console.log(action);
     },
   },

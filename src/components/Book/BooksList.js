0a146b35673d090dc06e49d50f0e2ae3c719e434
MohamedMamdouh-98 <miido96@gmail.com>
2022-09-20 16:42:40 +0200

    [handle]form submit & [create]insert data

--- src/components/Book/BooksList.js
+++ src/components/Book/BooksList.js
@@ -2,22 +2,24 @@ import React from "react";
 
 const BooksList = ({ isLoading, books }) => {
   const bookList =
-    books.map((item) => (
-      <li
-        key={item.id}
-        className="list-group-item d-flex  justify-content-between align-items-center"
-      >
-        <div>{item.title}</div>
-        <div className="btn-group" role="group">
-          <button type="button" className="btn btn-primary">
-            Read
-          </button>
-          <button type="button" className="btn btn-danger">
-            Delete
-          </button>
-        </div>
-      </li>
-    ));
+    books.length > 0
+      ? books.map((item) => (
+          <li
+            key={item.id}
+            className="list-group-item d-flex  justify-content-between align-items-center"
+          >
+            <div>{item.title}</div>
+            <div className="btn-group" role="group">
+              <button type="button" className="btn btn-primary">
+                Read
+              </button>
+              <button type="button" className="btn btn-danger">
+                Delete
+              </button>
+            </div>
+          </li>
+        ))
+      : "Ops No books";
   return (
     <div>
       <h2>Books List</h2>
--- src/store/bookSlice.js
+++ src/store/bookSlice.js
@@ -1,12 +1,31 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-//call Api
+//call Api & get data from json server
 export const getBooks = createAsyncThunk(
   "book/getBooks",
   async (_, thunkAPI) => {
-  const {rejectWithValue}= thunkAPI
+    const { rejectWithValue } = thunkAPI;
     try {
       const res = await fetch("http://localhost:3005/books");
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+//create thunk insert book
+export const insertBook = createAsyncThunk(
+  "book/insertBooks",
+  async (bookData, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    try {
+      const res = await fetch("http://localhost:3005/books", {
+        method: "POST",
+        body: JSON.stringify(bookData),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      });
       const data = await res.json();
       return data;
     } catch (error) {
@@ -20,9 +39,10 @@ const bookSlice = createSlice({
   name: "book",
   initialState: { books: [], isLoading: false, error: null },
   extraReducers: {
+    //get books
     [getBooks.pending]: (state, action) => {
       state.isLoading = true;
-      state.error = null
+      state.error = null;
     },
 
     [getBooks.fulfilled]: (state, action) => {
@@ -31,6 +51,19 @@ const bookSlice = createSlice({
     },
 
     [getBooks.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    //insert insert books
+    [insertBook.pending]: (state, action) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    [insertBook.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.books.push(action.payload);
+    },
+    [insertBook.rejected]: (state, action) => {
       state.isLoading = false;
       state.error = action.payload;
     },
